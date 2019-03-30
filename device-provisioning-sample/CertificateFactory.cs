abc00f039ef9d9375669f6bde9586a28bb738899
MattHoneycutt <mbhoneycutt@gmail.com>
2019-03-30 18:51:26 -0500

    Output full paths of generated certs.  Generate unique(-ish) cert name

--- device-provisioning-sample/CertificateFactory.cs
+++ device-provisioning-sample/CertificateFactory.cs
@@ -9,17 +9,21 @@ namespace DeviceProvisioningSample
     {
         public static void CreateTestCert()
         {
+            var ticks = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             var ecdsa = ECDsa.Create(); 
-            var request = new CertificateRequest("cn=globomantics-prov-dev-01", ecdsa, HashAlgorithmName.SHA256);
+            var request = new CertificateRequest($"cn=globomantics-dev-{ticks}", ecdsa, HashAlgorithmName.SHA256);
             var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(1));
 
             // Create PFX (PKCS #12) with private key
-            Console.WriteLine("Writing private key to key.pfx...");
-            File.WriteAllBytes("key.pfx", certificate.Export(X509ContentType.Pfx));
+            var keyPath = Path.GetFullPath("key.pfx");
+            Console.WriteLine($"Writing private key to {keyPath}...");
+            File.WriteAllBytes(keyPath, certificate.Export(X509ContentType.Pfx));
+
 
             // Create Base 64 encoded CER (public key only)
-            Console.WriteLine("Writing public certificate to cert.cer...");
-            File.WriteAllText("cert.cer", "-----BEGIN CERTIFICATE-----\r\n"
+            var certPath = Path.GetFullPath("cert.cer");
+            Console.WriteLine($"Writing public certificate to {certPath}...");
+            File.WriteAllText(certPath, "-----BEGIN CERTIFICATE-----\r\n"
                 + Convert.ToBase64String(certificate.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks)
                 + "\r\n-----END CERTIFICATE-----");
     
